597190998f6f3682a5470d759fa592650e6c303f
MFHSchool <119766714+MFHSchool@users.noreply.github.com>
2024-08-01 17:31:02 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,33 +1,37 @@
-// Add your code here
+enum LightCondition {
+    //% block="above"
+    Above,
+    //% block="below"
+    Below
+}
 
-namespace custom {
-    //% block
-    export function makeEnemySprite(count: number) {
-        for (let i = 0; i < count; i++) {
-            let enemySprite = sprites.createProjectileFromSide(img`
-                    . . . . . . . . . . . c c . . .
-                    . . . . . . . c c c c 6 3 c . .
-                    . . . . . . c 6 3 3 3 3 6 c . .
-                    . . c c . c 6 c c 3 3 3 3 3 c .
-                    . b 5 5 c 6 c 5 5 c 3 3 3 3 3 c
-                    . f f 5 c 6 c 5 f f 3 3 3 3 3 c
-                    . f f 5 c 6 c 5 f f 6 3 3 3 c c
-                    . b 5 5 3 c 3 5 5 c 6 6 6 6 c c
-                    . . b 5 5 3 5 5 c 3 3 3 3 3 3 c
-                    . c c 5 5 5 5 5 b c c 3 3 3 3 c
-                    c 5 5 4 5 5 5 4 b 5 5 c 3 3 c .
-                    b 5 4 b 4 4 4 4 b b 5 c b b . .
-                    c 4 5 5 b 4 b 5 5 5 4 c 4 5 b .
-                    c 5 5 5 c 4 c 5 5 5 c 4 c 5 c .
-                    c 5 5 5 5 c 5 5 5 5 c 4 c 5 c .
-                    . c c c c c c c c c . . c c c .
-                `, -70, 0)
-            enemySprite.y = randint(0, scene.screenHeight())
+let lightThreshold = 128
+let checkAbove = true
+
+//% block="set light threshold to $value"
+//% category="Input"
+//% value.min=0 value.max=255 value.defl=128
+function setLightThreshold(value: number) {
+    lightThreshold = value
+}
+
+//% block="check if light level is $condition threshold"
+//% category="Input"
+//% condition.shadow="dropdown" condition.options="LightCondition"
+function onLightLevelCheck(condition: LightCondition, handler: () => void) {
+    checkAbove = (condition == LightCondition.Above)
+    control.inBackground(function() {
+        while (true) {
+            if (checkAbove) {
+                if (input.lightLevel() > lightThreshold) {
+                    handler()
+                }
+            } else {
+                if (input.lightLevel() <= lightThreshold) {
+                    handler()
+                }
+            }
+            basic.pause(100) // Check every 100ms
         }
-    }
-
-    //% block
-    export function makeRandomBackground() {
-        scene.setBackgroundColor(randint(0, 15))
-    }
+    })
 }
